a80914c182275cdc5879a828892aab090540d824
Rebwon <msolo021015@naver.com>
2021-05-11 17:08:22 +0900

    refactor java8 version

--- src/test/java/com/rebwon/design/chap2/practice/CartForSongs.java
+++ src/test/java/com/rebwon/design/chap2/practice/CartForSongs.java
@@ -1,21 +1,15 @@
 package com.rebwon.design.chap2.practice;
 
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.List;
 
 public final class CartForSongs {
-    private ArrayList<Song> cart = new ArrayList<>();
+    private List<Song> cart = new ArrayList<>();
 
     public double calculateTotalPrice() {
-        double total = 0.0;
-        Iterator<Song> iterator = cart.iterator();
-
-        while(iterator.hasNext()) {
-            Song s = iterator.next();
-            total += s.calculatePrice();
-        }
-
-        return total;
+        return cart.stream()
+            .mapToDouble(Song::calculatePrice)
+            .sum();
     }
 
     public void add(Song song) {
