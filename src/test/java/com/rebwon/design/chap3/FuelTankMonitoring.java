5184b9b6c31524eca32b4bc22328e3d55433f414
Rebwon <msolo021015@naver.com>
2021-05-12 17:02:27 +0900

    abstraction fuelTankMonitoring

--- src/test/java/com/rebwon/design/chap3/FuelTankMonitoring.java
+++ src/test/java/com/rebwon/design/chap3/FuelTankMonitoring.java
@@ -1,19 +1,26 @@
 package com.rebwon.design.chap3;
 
 public final class FuelTankMonitoring {
+    // 1번 FuelTank를 만들어서, 위임
+    // 2번 FuelTank를 만들어서 연료 검사하고, 발송은 다른 방식.
+    private WaringSignal signal;
+    private FuelTank fuelTank;
 
     public void checkAndWarn() {
 
-        if(checkFuelTank()) {
-            giveWaringSignal();
+        if(fuelTank.checkFuelTank()) {  // 연료를 검사하는 기능
+            signal.send();          // 에러를 발송하는 기능
         }
     }
 
-    private void giveWaringSignal() {
-
+    interface WaringSignal {
+        void send();
     }
 
-    private boolean checkFuelTank() {
-        return false;
+    public static class FuelTank {
+
+        public boolean checkFuelTank() {
+            return true;
+        }
     }
 }
