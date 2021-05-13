a95b87c062d5c5bd0ce7cef5f0d6e3a50d78fda0
Rebwon <msolo021015@naver.com>
2021-05-13 09:43:38 +0900

    quiz clear

--- src/test/java/com/rebwon/design/chap3/TimeReminder.java
+++ src/test/java/com/rebwon/design/chap3/TimeReminder.java
@@ -3,14 +3,16 @@ package com.rebwon.design.chap3;
 import java.util.Calendar;
 
 public final class TimeReminder {
-    private Mp3 m;
+    private final Mp3 m;
+    private final TimeProvider provider;
+
+    public TimeReminder(Mp3 m, TimeProvider provider) {
+        this.m = m;
+        this.provider = provider;
+    }
 
     public void reminder() {
-        Calendar calendar = Calendar.getInstance();
-        m = new Mp3();
-
-        int hour = calendar.get(Calendar.HOUR_OF_DAY);
-
+        int hour = provider.getTime();
         if(hour >= 22)
             m.playSong();
     }
@@ -19,6 +21,34 @@ public final class TimeReminder {
 
         public void playSong() {
             System.out.println("Playing");
+        }
+    }
+
+    interface TimeProvider {
+        void setHours(int hours);
+        int getTime();
+    }
+
+    public static class FakeTimeProvider implements TimeProvider {
+        private Calendar cal;
+
+        public FakeTimeProvider() {
+            this.cal = Calendar.getInstance();
+        }
+
+        public FakeTimeProvider(int hours) {
+            this.cal = Calendar.getInstance();
+            setHours(hours);
+        }
+
+        @Override
+        public void setHours(int hours) {
+            cal.set(Calendar.HOUR_OF_DAY, hours);
+        }
+
+        @Override
+        public int getTime() {
+            return cal.get(Calendar.HOUR_OF_DAY);
         }
     }
 }
--- src/test/java/com/rebwon/design/chap3/TimeReminderTest.java
+++ src/test/java/com/rebwon/design/chap3/TimeReminderTest.java
@@ -1,14 +1,14 @@
 package com.rebwon.design.chap3;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import com.rebwon.design.chap3.TimeReminder.FakeTimeProvider;
+import com.rebwon.design.chap3.TimeReminder.Mp3;
 import org.junit.jupiter.api.Test;
 
 class TimeReminderTest {
 
     @Test
     void name() {
-        TimeReminder reminder = new TimeReminder();
+        TimeReminder reminder = new TimeReminder(new Mp3(), new FakeTimeProvider(22));
         reminder.reminder();
     }
 }
